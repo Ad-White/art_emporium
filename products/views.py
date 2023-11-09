d5ce03d337cdf4aefa2b76b17f8d0e5aad485fe3
Ad-White <>
2023-11-09 19:06:10 +0000

    Add product_detail view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -15,13 +15,14 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
-    def all_products(request):
+
+def product_detail(request, product_id):
     """ A view to show all products, plus searching and sorting queries """
 
-    products = Product.objects.all()
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': products,
+        'product': product,
     }
 
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/product_detail.html', context)
