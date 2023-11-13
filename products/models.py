81f21f3191962943283c0a34386e05c56ee9976a
Ad-White <>
2023-11-13 11:09:00 +0000

    Format code in models.py

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
 
     class Meta:
@@ -10,13 +11,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
- 
+
     def get_friendly_name(self):
         return self.friendly_name
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     artist = models.CharField(max_length=254, null=True, blank=True)
